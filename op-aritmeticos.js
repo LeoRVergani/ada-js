be33e1f6c6b32f4dde913ff5c015462c4500d458
leorvergani <vergani@outlook.com.br>
2024-08-12 02:06:46 -0300

    update

---
+++ op-aritmeticos.js
@@ -0,0 +1,14 @@
+let numero1 = 20;
+let numero2 = 2;
+
+const soma = numero1 + numero2;
+
+console.log("Soma = ", soma);
+console.log("Subtração = ", numero1 - numero2);
+console.log("Multiplicação = ", numero1 * numero2);
+console.log("Divisão = ", numero1 / numero2);
+console.log("Exponenciação = ", numero1 ** numero2);
+console.log("Resto de divisão = ", numero1 % 3);
+
+console.log(Math.PI);
+console.log(Math.sqrt(16));
---
+++ segunda-aula.js
@@ -0,0 +1,29 @@
+// variaveis
+
+let idade = 26;
+
+console.log(idade)
+
+// Tipos de variaveis
+
+let numero = 30;
+
+idade = 36;
+
+let altura = 1.80;
+let nome = "Leonardo";
+let estudando = true;
+
+console.log(idade, typeof idade);
+console.log(altura, typeof altura);
+console.log(nome, typeof nome);
+console.log(estudando, typeof estudando);
+
+let outra;
+
+console.log(outra);
+
+let nula = null;
+
+console.log(nula);
+
