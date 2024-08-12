6c7b7947d0a9a2ed8567743d1113c52b64090b55
leorvergani <vergani@outlook.com.br>
2024-08-12 03:39:46 -0300

    treinamento de grid

---
+++ op_boleanos.js
@@ -0,0 +1,11 @@
+console.log(10 != '10');
+console.log(10 !== '10');
+console.log(10 == '10');
+console.log(10 === '10');
+
+const idade = 36;
+
+const votoFacultavivo = idade > 18 || idade >= 70;
+
+console.log('Posso votar? ', votoFacultavivo);
+
